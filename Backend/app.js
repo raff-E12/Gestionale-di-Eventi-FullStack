586dbba4f7a4eeb9f7d6c8005da2bd8506c92e7d
Raff_12 <raffelecoppola381@gmail.com>
2025-06-08 18:54:11 +0200

    feat: iniziazione del backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,15 @@
 import express from "express"
 import cors from "cors"
 
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
-const connection = express();
-connection.use(connection.json());
+// Rotte Principali e Middlware (404 - Prevents)
+app.get("/", (req, res) =>{
+    return res.status(200).send({msg: "Benvenuto nella Calendar-API", code: 200});
+})
+
+app.listen(port, () =>{return console.log(`>> Iniziazione del host locale http://localhost:${port}`)})
