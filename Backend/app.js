7ff169d94c21ce1f9346d4747c433207ba0843b3
Raff_12 <raffelecoppola381@gmail.com>
2025-06-08 19:07:51 +0200

    feat: Modifica della cartella....

--- Backend/app.js
+++ Backend/app.js
@@ -1,15 +1,16 @@
 import express from "express"
 import cors from "cors"
+import dotenv from "dotenv"
 
+dotenv.config({path:"./assets/env/.env"});
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json());
-app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 // Rotte Principali e Middlware (404 - Prevents)
 app.get("/", (req, res) =>{
-    return res.status(200).send({msg: "Benvenuto nella Calendar-API", code: 200});
+    return res.status(200).send({msg: "Benvenuto nella Planify-Api", code: 200});
 })
 
 app.listen(port, () =>{return console.log(`>> Iniziazione del host locale http://localhost:${port}`)})
